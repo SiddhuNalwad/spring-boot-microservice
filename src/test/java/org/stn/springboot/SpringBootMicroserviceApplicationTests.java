379165438653f7a852f8f42670afe6d8ebee149d
Siddhu Nalwad <siddhu.gauri.nalwad@gmail.com>
2021-05-30 21:56:13 +0530

    Microservice using Spring Boot
    Develop a microservice using Spring Boot.

---
+++ src/test/java/org/stn/springboot/SpringBootMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stn.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
